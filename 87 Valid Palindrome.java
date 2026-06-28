fcf2b0af9069237b24f6659baa97bf82eb66f300
Aditya Dhar Dwivedi <adityadwivedi26oct@gmail.com>
2026-06-28 08:21:39 +0530

    Add files via upload

---
+++ 87 Valid Palindrome.java
@@ -0,0 +1,15 @@
+//import java.util.Scanner;
+//public class Main {
+//    static void main() {
+//        Scanner sc = new Scanner(System.in);
+//        String a = sc.nextLine();
+//        String reversedWord = new StringBuilder(a).reverse().toString();
+//        if (a.equalsIgnoreCase(reversedWord)){
+//            System.out.println("YES " +a+ " is a palindrome!");
+//        }else{
+//            System.out.println("NO " +a+ " is not a palindrome!");
+//        }
+//        sc.close();
+//    }
+//
+//}
