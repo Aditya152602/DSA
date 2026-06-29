b2ce7917f96b94677cb3987c9b1d581ed24e1089
Aditya Dhar Dwivedi <adityadwivedi26oct@gmail.com>
2026-06-29 08:07:05 +0530

    Add files via upload

---
+++ 88 Reverse String.java
@@ -0,0 +1,13 @@
+//import java.util.Scanner;
+//public class Main {
+//    static void main() {
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Enter a word");
+//        String a = sc.nextLine();
+//        System.out.println("Reversed");
+//        for (int i = a.length()-1;i>=0;i--){
+//            System.out.println(a.charAt(i));
+//        }
+//    }
+//
+//}
