4a71936a4b101d30f6ea5c2585398932af113559
Aditya Dhar Dwivedi <adityadwivedi26oct@gmail.com>
2026-06-30 08:45:50 +0530

    Add files via upload

---
+++ 89 Reverse Words in a String.java
@@ -0,0 +1,16 @@
+//import java.util.Scanner;
+//public class Main {
+//    static void main() {
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Enter a Text");
+//        String a = sc.nextLine();
+//        String[] words = a.split(" ");
+//        System.out.println("Reversed: ");
+//        for (int i= words.length-1;i>=0; i--){
+//            String currentWord = words[i];
+//            System.out.println(currentWord+ " ");
+//        }
+//
+//    }
+//
+//}
