a6f0a69462b5eb4ceb15ed54a4c41c964c9f77f2
Aditya Dhar Dwivedi <adityadwivedi26oct@gmail.com>
2026-07-02 08:43:44 +0530

    Add files via upload

---
+++ 91 Implement strStr.java
@@ -0,0 +1,19 @@
+//public class Main {
+//    public int strStr(String haystack,String needle){
+//        for(int i =0; i<=haystack.length()-needle.length();i++){
+//            String part = haystack.substring(i,i+needle.length());
+//            if(part.equals(needle)){
+//                return i ;
+//            }
+//        }
+//        return -1;
+//    }
+//
+//    static void main() {
+//        String haystack = "Hello";
+//        String needle ="ll";
+//        Main obj = new Main();
+//        int index = obj.strStr(haystack,needle);
+//        System.out.println("Index: "  + index);
+//    }
+//}
