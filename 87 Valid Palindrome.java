6b9f6a369a64446cd55ca34b6b09c73bf5494946
Aditya Dhar Dwivedi <adityadwivedi26oct@gmail.com>
2026-08-05 18:28:56 +0530

    Update 87 Valid Palindrome.java

--- 87 Valid Palindrome.java
+++ 87 Valid Palindrome.java
@@ -1,15 +1,23 @@
-//import java.util.Scanner;
-//public class Main {
-//    static void main() {
-//        Scanner sc = new Scanner(System.in);
-//        String a = sc.nextLine();
-//        String reversedWord = new StringBuilder(a).reverse().toString();
-//        if (a.equalsIgnoreCase(reversedWord)){
-//            System.out.println("YES " +a+ " is a palindrome!");
-//        }else{
-//            System.out.println("NO " +a+ " is not a palindrome!");
-//        }
-//        sc.close();
-//    }
-//
-//}
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.nextLine();
+        
+        // Remove all non-alphanumeric characters (spaces, punctuation)
+        String cleanedWord = a.replaceAll("[^a-zA-Z0-9]", "");
+        
+        // Reverse the cleaned word
+        String reversedWord = new StringBuilder(cleanedWord).reverse().toString();
+        
+        // Compare them
+        if (cleanedWord.equalsIgnoreCase(reversedWord)) {
+            System.out.println("YES, \"" + a + "\" is a palindrome!");
+        } else {
+            System.out.println("NO, \"" + a + "\" is not a palindrome!");
+        }
+        
+        sc.close();
+    }
+}
